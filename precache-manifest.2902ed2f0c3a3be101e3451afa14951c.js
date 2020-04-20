cbc4adb237dbe2b09cd36c9d5fc0247652aad334
dimon <dimagol64@gmail.com>
2020-04-20 21:13:04 +0300

    Updates

---
+++ precache-manifest.2902ed2f0c3a3be101e3451afa14951c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "752c892c45702db2a453",
+    "url": "/react-gotap-db/static/js/runtime~main.1eab07cd.js"
+  },
+  {
+    "revision": "719d225031478f6cc570",
+    "url": "/react-gotap-db/static/js/main.db4616d3.chunk.js"
+  },
+  {
+    "revision": "1d54a84c95dc0c98b679",
+    "url": "/react-gotap-db/static/js/2.7cc0acb3.chunk.js"
+  },
+  {
+    "revision": "719d225031478f6cc570",
+    "url": "/react-gotap-db/static/css/main.81991616.chunk.css"
+  },
+  {
+    "revision": "1d54a84c95dc0c98b679",
+    "url": "/react-gotap-db/static/css/2.02e1e0eb.chunk.css"
+  },
+  {
+    "revision": "705b191dd0bac224c4cc2175b4d9f2e6",
+    "url": "/react-gotap-db/index.html"
+  }
+];
